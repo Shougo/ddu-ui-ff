65c2efaf096aa7f75bbeec9bda9034e3d697f8e9
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-07-27 19:26:05 +0900

    Update deps

--- denops/@ddu-ui-ff/preview.ts
+++ denops/@ddu-ui-ff/preview.ts
@@ -8,14 +8,14 @@ import {
   PreviewContext,
   Previewer,
   TerminalPreviewer,
-} from "https://deno.land/x/ddu_vim@v1.8.7/types.ts";
+} from "https://deno.land/x/ddu_vim@v1.8.8/types.ts";
 import {
   batch,
   Denops,
   ensureObject,
   fn,
-} from "https://deno.land/x/ddu_vim@v1.8.7/deps.ts";
-import { replace } from "https://deno.land/x/denops_std@v3.3.2/buffer/mod.ts";
+} from "https://deno.land/x/ddu_vim@v1.8.8/deps.ts";
+import { replace } from "https://deno.land/x/denops_std@v3.5.0/buffer/mod.ts";
 import { Params } from "../@ddu-uis/ff.ts";
 
 type PreviewParams = {
