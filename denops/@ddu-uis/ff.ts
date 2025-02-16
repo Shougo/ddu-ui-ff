03f3f0411235e56139e34af6813aa68e13a5df38
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-02-16 14:21:20 +0900

    Use the latest ddu

--- denops/@ddu-uis/ff.ts
+++ denops/@ddu-uis/ff.ts
@@ -7,9 +7,9 @@ import {
   type PreviewContext,
   type Previewer,
   type UiOptions,
-} from "jsr:@shougo/ddu-vim@~9.5.0/types";
-import { BaseUi, type UiActions } from "jsr:@shougo/ddu-vim@~9.5.0/ui";
-import { printError } from "jsr:@shougo/ddu-vim@~9.5.0/utils";
+} from "jsr:@shougo/ddu-vim@~10.0.0/types";
+import { BaseUi, type UiActions } from "jsr:@shougo/ddu-vim@~10.0.0/ui";
+import { printError } from "jsr:@shougo/ddu-vim@~10.0.0/utils";
 
 import type { Denops } from "jsr:@denops/std@~7.4.0";
 import { batch } from "jsr:@denops/std@~7.4.0/batch";
