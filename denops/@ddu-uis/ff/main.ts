7d4faee376a3e952463cfa0b6f98026368b8b0db
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-09-16 12:33:17 +0900

    Improve

--- denops/@ddu-uis/ff/main.ts
+++ denops/@ddu-uis/ff/main.ts
@@ -1684,7 +1684,8 @@ export class Ui extends BaseUi<Params> {
 
           const prevName = await fn.bufname(args.denops, args.context.bufNr);
           await args.denops.cmd(
-            prevName !== args.context.bufName || args.context.bufNr == bufnr
+            prevName !== args.context.bufName || args.context.bufNr == bufnr ||
+              args.context.bufNr <= 0
               ? "enew"
               : `buffer ${args.context.bufNr}`,
           );
